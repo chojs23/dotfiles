a66059bf23efb8fa7d62a130790c65c2539966ba
Neo <chojs990222@gmail.com>
2026-07-15 19:55:29 +0900

    Add newline keymap

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,7 @@ local act = wezterm.action
 local config = {}
 -- Use config builder object if possible
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 config.font = wezterm.font("Iosevka Nerd Font Mono")
@@ -25,25 +25,25 @@ config.enable_kitty_keyboard = true
 config.tab_max_width = 20
 
 config.colors = {
-	cursor_bg = "#FFFF00",
-	cursor_border = "#ffffff",
-	cursor_fg = "#000000",
+  cursor_bg = "#FFFF00",
+  cursor_border = "#ffffff",
+  cursor_fg = "#000000",
 }
 
 config.color_scheme = "deep"
 config.font = wezterm.font_with_fallback({
-	{
-		family = "Iosevka Nerd Font Mono",
-		-- family = "IosevkaTermSlab Nerd Font Mono",
-		scale = 1.1,
-		weight = "Light",
-		harfbuzz_features = {
-			"calt=0",
-			"clig=0",
-			"liga=0",
-		},
-	},
-	-- { family = "CaskaydiaCove Nerd Font", scale = 1.1 },
+  {
+    family = "Iosevka Nerd Font Mono",
+    -- family = "IosevkaTermSlab Nerd Font Mono",
+    scale = 1.1,
+    weight = "Light",
+    harfbuzz_features = {
+      "calt=0",
+      "clig=0",
+      "liga=0",
+    },
+  },
+  -- { family = "CaskaydiaCove Nerd Font", scale = 1.1 },
 })
 -- config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
@@ -53,15 +53,15 @@ config.default_workspace = "main"
 
 -- Dim inactive panes
 config.inactive_pane_hsb = {
-	saturation = 0.24,
-	brightness = 0.5,
+  saturation = 0.24,
+  brightness = 0.5,
 }
 
 -- navigator
 -- if you are *NOT* lazy-loading smart-splits.nvim (recommended)
 local function is_vim(pane)
-	-- this is set by the plugin, and unset on ExitPre in Neovim
-	return pane:get_user_vars().IS_NVIM == "true"
+  -- this is set by the plugin, and unset on ExitPre in Neovim
+  return pane:get_user_vars().IS_NVIM == "true"
 end
 
 -- if you *ARE* lazy-loading smart-splits.nvim (not recommended)
@@ -79,136 +79,131 @@ end
 -- end
 
 local direction_keys = {
-	h = "Left",
-	j = "Down",
-	k = "Up",
-	l = "Right",
+  h = "Left",
+  j = "Down",
+  k = "Up",
+  l = "Right",
 }
 
 local function split_nav(resize_or_move, key)
-	return {
-		key = key,
-		mods = resize_or_move == "resize" and "META" or "CTRL",
-		action = wezterm.action_callback(function(win, pane)
-			if is_vim(pane) then
-				-- pass the keys through to vim/nvim
-				win:perform_action({
-					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
-				}, pane)
-			else
-				if resize_or_move == "resize" then
-					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-				else
-					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-				end
-			end
-		end),
-	}
+  return {
+    key = key,
+    mods = resize_or_move == "resize" and "META" or "CTRL",
+    action = wezterm.action_callback(function(win, pane)
+      if is_vim(pane) then
+        -- pass the keys through to vim/nvim
+        win:perform_action({
+          SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+        }, pane)
+      else
+        if resize_or_move == "resize" then
+          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+        end
+      end
+    end),
+  }
 end
 
 -- Keys
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-	-- Send C-a when pressing C-a twice
-	{ key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
-	{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
-	{ key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
+  -- Send C-a when pressing C-a twice
+  { key = "a",          mods = "LEADER|CTRL",  action = act.SendKey({ key = "a", mods = "CTRL" }) },
+  { key = "c",          mods = "LEADER",       action = act.ActivateCopyMode },
+  { key = "phys:Space", mods = "LEADER",       action = act.ActivateCommandPalette },
 
-	-- Send a newline when pressing SHIFT+ENTER
-	{
-		key = "Enter",
-		mods = "SHIFT",
-		action = wezterm.action.SendString("\x0a"),
-	},
+  { key = "Enter",      mods = "SHIFT",        action = wezterm.action.SendString("\x1b\r") },
 
-	-- Pane keybindings
-	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
-	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
-	{ key = "o", mods = "LEADER", action = act.RotatePanes("Clockwise") },
-	-- We can make separate keybindings for resizing panes
-	-- But Wezterm offers custom "mode" in the name of "KeyTable"
-	{
-		key = "r",
-		mods = "LEADER",
-		action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
-	},
+  -- Pane keybindings
+  { key = "-",          mods = "LEADER",       action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "|",          mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "x",          mods = "LEADER",       action = act.CloseCurrentPane({ confirm = true }) },
+  { key = "z",          mods = "LEADER",       action = act.TogglePaneZoomState },
+  { key = "o",          mods = "LEADER",       action = act.RotatePanes("Clockwise") },
+  -- We can make separate keybindings for resizing panes
+  -- But Wezterm offers custom "mode" in the name of "KeyTable"
+  {
+    key = "r",
+    mods = "LEADER",
+    action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
+  },
 
-	-- Back to last tab
-	{
-		key = "phys:Tab",
-		mods = "CTRL",
-		action = wezterm.action.ActivateLastTab,
-	},
+  -- Back to last tab
+  {
+    key = "phys:Tab",
+    mods = "CTRL",
+    action = wezterm.action.ActivateLastTab,
+  },
 
-	-- move between split panes
-	split_nav("move", "h"),
-	split_nav("move", "j"),
-	split_nav("move", "k"),
-	split_nav("move", "l"),
-	-- resize panes
-	split_nav("resize", "h"),
-	split_nav("resize", "j"),
-	split_nav("resize", "k"),
-	split_nav("resize", "l"),
+  -- move between split panes
+  split_nav("move", "h"),
+  split_nav("move", "j"),
+  split_nav("move", "k"),
+  split_nav("move", "l"),
+  -- resize panes
+  split_nav("resize", "h"),
+  split_nav("resize", "j"),
+  split_nav("resize", "k"),
+  split_nav("resize", "l"),
 
-	-- Tab keybindings
-	{ key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
-	{ key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
-	{ key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
-	{ key = "w", mods = "LEADER", action = act.ShowTabNavigator },
-	{
-		key = "e",
-		mods = "LEADER",
-		action = act.PromptInputLine({
-			description = wezterm.format({
-				{ Attribute = { Intensity = "Bold" } },
-				{ Foreground = { AnsiColor = "Fuchsia" } },
-				{ Text = "Renaming Tab Title...:" },
-			}),
-			action = wezterm.action_callback(function(window, pane, line)
-				if line then
-					window:active_tab():set_title(line)
-				end
-			end),
-		}),
-	},
-	-- Key table for moving tabs around
-	{ key = "m", mods = "LEADER", action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
-	-- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
-	{ key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
-	{ key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
+  -- Tab keybindings
+  { key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+  { key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+  { key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+  { key = "w", mods = "LEADER", action = act.ShowTabNavigator },
+  {
+    key = "e",
+    mods = "LEADER",
+    action = act.PromptInputLine({
+      description = wezterm.format({
+        { Attribute = { Intensity = "Bold" } },
+        { Foreground = { AnsiColor = "Fuchsia" } },
+        { Text = "Renaming Tab Title...:" },
+      }),
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    }),
+  },
+  -- Key table for moving tabs around
+  { key = "m", mods = "LEADER",       action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
+  -- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
+  { key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
+  { key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
 
-	-- Lastly, workspace
-	{ key = "W", mods = "LEADER|SHIFT", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+  -- Lastly, workspace
+  { key = "W", mods = "LEADER|SHIFT", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
 }
 -- I can use the tab navigator (LDR t), but I also want to quickly navigate tabs with index
 for i = 1, 9 do
-	table.insert(config.keys, {
-		key = tostring(i),
-		mods = "LEADER",
-		action = act.ActivateTab(i - 1),
-	})
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = "LEADER",
+    action = act.ActivateTab(i - 1),
+  })
 end
 
 config.key_tables = {
-	resize_pane = {
-		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
-		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
-		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
-		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
-		{ key = "Escape", action = "PopKeyTable" },
-		{ key = "Enter", action = "PopKeyTable" },
-	},
-	move_tab = {
-		{ key = "h", action = act.MoveTabRelative(-1) },
-		{ key = "j", action = act.MoveTabRelative(-1) },
-		{ key = "k", action = act.MoveTabRelative(1) },
-		{ key = "l", action = act.MoveTabRelative(1) },
-		{ key = "Escape", action = "PopKeyTable" },
-		{ key = "Enter", action = "PopKeyTable" },
-	},
+  resize_pane = {
+    { key = "h",      action = act.AdjustPaneSize({ "Left", 1 }) },
+    { key = "j",      action = act.AdjustPaneSize({ "Down", 1 }) },
+    { key = "k",      action = act.AdjustPaneSize({ "Up", 1 }) },
+    { key = "l",      action = act.AdjustPaneSize({ "Right", 1 }) },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  },
+  move_tab = {
+    { key = "h",      action = act.MoveTabRelative(-1) },
+    { key = "j",      action = act.MoveTabRelative(-1) },
+    { key = "k",      action = act.MoveTabRelative(1) },
+    { key = "l",      action = act.MoveTabRelative(1) },
+    { key = "Escape", action = "PopKeyTable" },
+    { key = "Enter",  action = "PopKeyTable" },
+  },
 }
 
 -- Tab bar
@@ -218,148 +213,148 @@ config.status_update_interval = 1000
 config.tab_bar_at_bottom = true
 
 local get_last_folder_segment = function(value)
-	if value == nil then
-		return "N/A"
-	end
+  if value == nil then
+    return "N/A"
+  end
 
-	local s = tostring(value)
+  local s = tostring(value)
 
-	if s == "" then
-		return "N/A"
-	end
+  if s == "" then
+    return "N/A"
+  end
 
-	local pathStripped = s:match("^file:///(.+)") or s
-	pathStripped = pathStripped:gsub("\\", "/")
+  local pathStripped = s:match("^file:///(.+)") or s
+  pathStripped = pathStripped:gsub("\\", "/")
 
-	local last = nil
-	for segment in string.gmatch(pathStripped, "[^/]+") do
-		last = segment
-	end
+  local last = nil
+  for segment in string.gmatch(pathStripped, "[^/]+") do
+    last = segment
+  end
 
-	return last or "N/A"
+  return last or "N/A"
 end
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local active_pane = tab.active_pane
+  local active_pane = tab.active_pane
 
-	local index = 0
-	if tab and tab.tab_index ~= nil then
-		index = tab.tab_index + 1
-	end
+  local index = 0
+  if tab and tab.tab_index ~= nil then
+    index = tab.tab_index + 1
+  end
 
-	local cwd = "N/A"
-	if active_pane and active_pane.current_working_dir then
-		local current_working_dir = active_pane.current_working_dir
+  local cwd = "N/A"
+  if active_pane and active_pane.current_working_dir then
+    local current_working_dir = active_pane.current_working_dir
 
-		if type(current_working_dir) == "userdata" then
-			cwd = current_working_dir.path or "N/A"
-		elseif type(current_working_dir) == "table" then
-			cwd = current_working_dir.path or "N/A"
-		else
-			cwd = tostring(current_working_dir)
-		end
-	end
+    if type(current_working_dir) == "userdata" then
+      cwd = current_working_dir.path or "N/A"
+    elseif type(current_working_dir) == "table" then
+      cwd = current_working_dir.path or "N/A"
+    else
+      cwd = tostring(current_working_dir)
+    end
+  end
 
-	local cmd = "N/A"
-	if active_pane and active_pane.foreground_process_name then
-		cmd = tostring(active_pane.foreground_process_name)
-	end
+  local cmd = "N/A"
+  if active_pane and active_pane.foreground_process_name then
+    cmd = tostring(active_pane.foreground_process_name)
+  end
 
-	local title = string.format(
-		"%s %s:%s",
-		tostring(index),
-		tostring(get_last_folder_segment(cwd) or "N/A"),
-		tostring(get_last_folder_segment(cmd) or "N/A")
-	)
+  local title = string.format(
+    "%s %s:%s",
+    tostring(index),
+    tostring(get_last_folder_segment(cwd) or "N/A"),
+    tostring(get_last_folder_segment(cmd) or "N/A")
+  )
 
-	return {
-		{ Text = " " .. title .. " " },
-	}
+  return {
+    { Text = " " .. title .. " " },
+  }
 end)
 
 wezterm.on("update-status", function(window, pane)
-	-- Workspace name
-	local stat = window:active_workspace()
-	local stat_color = "#f7768e"
-	-- It's a little silly to have workspace name all the time
-	-- Utilize this to display LDR or current key table name
-	if window:active_key_table() then
-		stat = window:active_key_table()
-		stat_color = "#7dcfff"
-	end
-	if window:leader_is_active() then
-		stat = "LDR"
-		stat_color = "#bb9af7"
-	end
+  -- Workspace name
+  local stat = window:active_workspace()
+  local stat_color = "#f7768e"
+  -- It's a little silly to have workspace name all the time
+  -- Utilize this to display LDR or current key table name
+  if window:active_key_table() then
+    stat = window:active_key_table()
+    stat_color = "#7dcfff"
+  end
+  if window:leader_is_active() then
+    stat = "LDR"
+    stat_color = "#bb9af7"
+  end
 
-	-- Current working directory
-	local basename = function(s)
-		-- Nothing a little regex can't fix
-		return string.gsub(s, "(.*[/\\])(.*)", "%2")
-	end
-	-- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l). Not a big deal, but check in case
-	local cwd_uri = pane:get_current_working_dir()
-	local cwd = ""
-	local hostname = ""
-	if cwd_uri then
-		if type(cwd_uri) == "userdata" then
-			-- Running on a newer version of wezterm and we have
-			-- a URL object here, making this simple!
-			cwd = cwd_uri.path
-			hostname = cwd_uri.host or wezterm.hostname()
-		else
-			-- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
-			-- which doesn't have the Url object
-			cwd_uri = cwd_uri:sub(8)
-			local slash = cwd_uri:find("/")
-			if slash then
-				hostname = cwd_uri:sub(1, slash - 1)
-				-- and extract the cwd from the uri, decoding %-encoding
-				cwd = cwd_uri:sub(slash):gsub("%%(%x%x)", function(hex)
-					return string.char(tonumber(hex, 16))
-				end)
-			end
-		end
+  -- Current working directory
+  local basename = function(s)
+    -- Nothing a little regex can't fix
+    return string.gsub(s, "(.*[/\\])(.*)", "%2")
+  end
+  -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l). Not a big deal, but check in case
+  local cwd_uri = pane:get_current_working_dir()
+  local cwd = ""
+  local hostname = ""
+  if cwd_uri then
+    if type(cwd_uri) == "userdata" then
+      -- Running on a newer version of wezterm and we have
+      -- a URL object here, making this simple!
+      cwd = cwd_uri.path
+      hostname = cwd_uri.host or wezterm.hostname()
+    else
+      -- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
+      -- which doesn't have the Url object
+      cwd_uri = cwd_uri:sub(8)
+      local slash = cwd_uri:find("/")
+      if slash then
+        hostname = cwd_uri:sub(1, slash - 1)
+        -- and extract the cwd from the uri, decoding %-encoding
+        cwd = cwd_uri:sub(slash):gsub("%%(%x%x)", function(hex)
+          return string.char(tonumber(hex, 16))
+        end)
+      end
+    end
 
-		-- Remove the domain name portion of the hostname
-		local dot = hostname:find("[.]")
-		if dot then
-			hostname = hostname:sub(1, dot - 1)
-		end
-		if hostname == "" then
-			hostname = wezterm.hostname()
-		end
-	end
+    -- Remove the domain name portion of the hostname
+    local dot = hostname:find("[.]")
+    if dot then
+      hostname = hostname:sub(1, dot - 1)
+    end
+    if hostname == "" then
+      hostname = wezterm.hostname()
+    end
+  end
 
-	-- cwd = cwd and basename(cwd) or ""
-	-- Current command
-	local cmd = pane:get_foreground_process_name()
-	cmd = cmd and basename(cmd) or ""
+  -- cwd = cwd and basename(cwd) or ""
+  -- Current command
+  local cmd = pane:get_foreground_process_name()
+  cmd = cmd and basename(cmd) or ""
 
-	-- Time
-	local time = wezterm.strftime_utc("%Y-%m-%d %H:%M:%S")
+  -- Time
+  local time = wezterm.strftime_utc("%Y-%m-%d %H:%M:%S")
 
-	-- Left status (left of the tab line)
-	window:set_left_status(wezterm.format({
-		{ Foreground = { Color = stat_color } },
-		{ Text = "  " },
-		{ Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
-		{ Text = " |" },
-	}))
+  -- Left status (left of the tab line)
+  window:set_left_status(wezterm.format({
+    { Foreground = { Color = stat_color } },
+    { Text = "  " },
+    { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+    { Text = " |" },
+  }))
 
-	-- Right status
-	window:set_right_status(wezterm.format({
-		{ Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
-		-- { Text = " | " },
-		-- { Text = wezterm.nerdfonts.md_folder .. "  " .. hostname },
-		-- { Text = " | " },
-		-- { Foreground = { Color = "#e0af68" } },
-		{ Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
-		"ResetAttributes",
-		{ Text = " | " },
-		{ Text = wezterm.nerdfonts.md_clock .. "  " .. time .. " UTC" },
-		{ Text = "  " },
-	}))
+  -- Right status
+  window:set_right_status(wezterm.format({
+    { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+    -- { Text = " | " },
+    -- { Text = wezterm.nerdfonts.md_folder .. "  " .. hostname },
+    -- { Text = " | " },
+    -- { Foreground = { Color = "#e0af68" } },
+    { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+    "ResetAttributes",
+    { Text = " | " },
+    { Text = wezterm.nerdfonts.md_clock .. "  " .. time .. " UTC" },
+    { Text = "  " },
+  }))
 end)
 
 return config
